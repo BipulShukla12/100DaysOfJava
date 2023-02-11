0f9efbf104c549308861084c578e1aa02c18109b
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-12 01:37:38 +0530

    Print no

---
+++ 100DaysOfJava/src/loop.java
@@ -0,0 +1,10 @@
+
+// print Numbers from 1 to 5.
+public class loop {
+      public static void main(String[] args) {
+            for(int i = 1; i <=5; i +=1){
+                  System.out.println( i );
+            }
+            
+      }
+}
