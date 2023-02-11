98b97caa356fad4faccd0515191b4c3bf7260bea
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-12 01:48:45 +0530

    no 1 to n

--- 100DaysOfJava/src/loop.java
+++ 100DaysOfJava/src/loop.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 
 // print Numbers from 1 to 5.
 public class loop {
       public static void main(String[] args) {
-            for(int i = 1; i <=5; i +=1){
-                  System.out.println( i );
+           // for(int i = 1; i <=5; i +=1){
+                 // System.out.println( i );
+            //}
+ //print number 1 to n.          
+            Scanner in = new Scanner (System.in);
+            int n = in.nextInt();
+
+            for(int i = 1; i<=n; i++){
+                  System.out.println(i + " ");
             }
-            
+
+
       }
 }
