d1ad33c492566afd813a29479048358f5e0d331e
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-12 01:55:59 +0530

    Infi

---
+++ 100DaysOfJava/src/Infi.java
@@ -0,0 +1,9 @@
+public class Infi {
+      public static void main(String args[]) {
+        int startseq=1,lastseq=1024;
+        while (startseq<=lastseq){
+            System.out.print(startseq+" ");
+            startseq=startseq*2;
+        }
+      }
+  }
