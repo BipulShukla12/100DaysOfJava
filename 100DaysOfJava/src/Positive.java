74395d55c96b14ecffe92e4593fd24b72c4c9099
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-13 00:40:51 +0530

    latest commit day3

--- 100DaysOfJava/src/Positive.java
+++ 100DaysOfJava/src/Positive.java
@@ -2,9 +2,9 @@ public class Positive {
       public static void main(String[] args) {
             int num =8;
             if(num<0)
-            System.out.println("number is positive");
+            System.out.println("number is Negative");
             else if(num>0)
-            System.out.println("Number is negative");
+            System.out.println("Number is positive");
             else
             System.out.println("zero");
       }
