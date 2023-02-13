4d549d49bbeb6f81180b5bb27bd5ac54a49c717b
Bipul Shukla <114582657+BipulShukla12@users.noreply.github.com>
2023-02-14 00:10:44 +0530

    Update Leapyear.java

--- 100DaysOfJava/src/Leapyear.java
+++ 100DaysOfJava/src/Leapyear.java
@@ -2,7 +2,7 @@ public class Leapyear{
    public static void main (String[]args){
 
      int year = 2021;
-       //leap year
+       
      if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
        System.out.println (year + " is a Leap Year");
 
