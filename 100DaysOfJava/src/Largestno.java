53dd65ea89f469d5541ead824a41a6ef1ae01178
Bipul Shukla <114582657+BipulShukla12@users.noreply.github.com>
2023-02-15 01:13:06 +0530

    Update Largestno.java

--- 100DaysOfJava/src/Largestno.java
+++ 100DaysOfJava/src/Largestno.java
@@ -1,10 +1,10 @@
 // Write a program to find the largest of two numbers in java
-public class Largestno
-{
-   public static void main (String[]args)
-   {
+public class Largestno{
+   
+   public static void main (String[]args){
+      
 
-     int num1 = 30, num2 = 70;
+     int num1 = 90, num2 = 70;
      if (num1 == num2)
        System.out.println ("both are equal");
      else if (num1 > num2)
