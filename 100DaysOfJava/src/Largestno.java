4a45ff026bb77d8ba86aa1458048e0e1e1e214c3
Bipul Shukla <114582657+BipulShukla12@users.noreply.github.com>
2023-02-15 01:10:47 +0530

    Update Largestno.java
    Day5

--- 100DaysOfJava/src/Largestno.java
+++ 100DaysOfJava/src/Largestno.java
@@ -9,7 +9,7 @@ public class Largestno
        System.out.println ("both are equal");
      else if (num1 > num2)
          System.out.println (num1 + " is greater");
-// Write a program to find the largest of two numbers in java
+
      else
          System.out.println (num2 + " is greater");
 
