d1b37907abd709122a7e36d519ed187c40188f9a
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-16 00:02:42 +0530

    Day6

---
+++ 100DaysOfJava/src/Hcf.java
@@ -0,0 +1,19 @@
+// hcf
+class Hcf{
+  public static void main (String[]args){
+    int num1 = 36, num2 = 60, hcf;
+
+    while (num1 != num2)
+    {
+        if (num1 > num2)
+            num1 -= num2;
+        else
+            num2 -= num1;
+    }
+//hhhhhhh
+
+
+    System.out.println("The HCF: "+ num1);
+  }
+
+}
