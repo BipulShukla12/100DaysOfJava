07538cc4a73e837dcdc6be23ac61a3ddb4810223
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-17 21:29:54 +0530

    day7

---
+++ 100DaysOfJava/src/Palindrome.java
@@ -0,0 +1,23 @@
+public class Palindrome{
+      
+   public static void main (String[]args)
+   {
+     //variables initialization
+     int num = 12021, reverse = 0, rem, temp;
+
+       temp = num;
+     //loop to find reverse number
+     while (temp != 0)
+       {
+     	rem = temp % 10;
+     	reverse = reverse * 10 + rem;
+     	temp /= 10;
+       };
+
+     // palindrome if num and reverse are equal
+     if (num == reverse)
+       System.out.println (num + " is Palindrome");
+     else
+       System.out.println (num + " is not Palindrome");
+   }
+ }
