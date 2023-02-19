19c8857d130f951be4b1ff314de0c734f87f84ae
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-19 21:34:10 +0530

    Day9

---
+++ 100DaysOfJava/src/Lcm.java
@@ -0,0 +1,21 @@
+public class Lcm{
+  public static void main (String[]args)
+  {
+    int num1 = 36, num2 = 60, lcm = 0;
+
+    // finding the larger number here
+    int max = (num1 > num2) ? num1 : num2;
+
+    // LCM will atleast be >= max(num1, num2)
+    // Largest possibility of LCM will be num1*num2
+    for (int i = max; i <= num1 * num2; i++)
+      {
+     if (i % num1 == 0 && i % num2 == 0)
+        {
+         lcm = i;
+         break;
+        }
+      }
+    System.out.println ("The LCM: " + lcm);
+  }
+}
