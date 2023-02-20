fff8b9cb87780d93bb724d449b4b02146fd95f65
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-20 21:07:42 +0530

    day10

---
+++ 100DaysOfJava/Perfectsq.java
@@ -0,0 +1,24 @@
+public class Perfectsq{
+
+	static boolean isPerfectSquare(int x)
+	{
+		if (x >= 0) {
+		
+			int sr = (int)Math.sqrt(x);
+		
+			return ((sr * sr) == x);
+		}
+		return false;
+	}
+////jjdhcjk
+//jdj
+	public static void main(String[] args)
+	{
+		int x = 84;
+
+		if (isPerfectSquare(x))
+			System.out.print("True");
+		else
+			System.out.print("False");
+	}
+}
