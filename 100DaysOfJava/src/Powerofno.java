1a87ffb7340b5b8b1a32ca38eb5adf8f49883049
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-21 19:50:34 +0530

    Day11

---
+++ 100DaysOfJava/src/Powerofno.java
@@ -0,0 +1,16 @@
+public class Powerofno{
+	public static void main(String[] args) {
+	    
+	double base = 2.5;
+    // Works only when exponent is positive integer
+    int expo = 2;
+    double res = 1.0;
+    
+    while (expo != 0) {
+        res *= base;
+        expo--;
+    }
+    
+    System.out.println("Result = " + res); 
+	}
+}
