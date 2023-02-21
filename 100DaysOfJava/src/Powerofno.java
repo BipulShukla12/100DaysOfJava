c62574558152e4f6df1b8f8c11f4fd4167af24d5
Bipul Shukla <114582657+BipulShukla12@users.noreply.github.com>
2023-02-21 19:51:52 +0530

    Update Powerofno.java

--- 100DaysOfJava/src/Powerofno.java
+++ 100DaysOfJava/src/Powerofno.java
@@ -2,7 +2,7 @@ public class Powerofno{
 	public static void main(String[] args) {
 	    
 	double base = 2.5;
-    // Works only when exponent is positive 
+    // Works only when exponent is  
     int expo = 2;
     double res = 1.0;
     
