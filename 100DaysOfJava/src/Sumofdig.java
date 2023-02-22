00f9f6b207d8aa7185c37a5a805a6a321c219a39
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-22 22:46:34 +0530

    day12

---
+++ 100DaysOfJava/src/Sumofdig.java
@@ -0,0 +1,20 @@
+public class Sumofdig{
+   public static void main (String[]args)
+   {
+
+     int num = 12345, sum = 0;
+
+
+
+     //loop to find sum of digits
+     while(num!=0){
+         sum += num % 10;
+         num = num / 10;
+     }
+
+     //output
+       System.out.println ("Sum of digits : " + sum);
+   }
+
+
+ }
