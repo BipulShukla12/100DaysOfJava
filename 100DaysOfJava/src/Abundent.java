b7e36ae9241788beb604166a2add02efcab4b603
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-23 22:22:53 +0530

    Day12

---
+++ 100DaysOfJava/src/Abundent.java
@@ -0,0 +1,18 @@
+public class Abundent{
+
+
+//Abundent no
+   public static void main (String[]args)
+   {
+
+     int n = 12, sum = 0;
+
+     for (int i = 1; i < n; i++) { if (n % i == 0) sum = sum + i; } if (sum > n)
+       {
+     	System.out.println (n + " is an Abundant Number");
+     	System.out.println ("The Abundance is: " + (sum - n));
+       }
+     else
+       System.out.println (n + " is not an Abundant Number");
+   }
+ }
