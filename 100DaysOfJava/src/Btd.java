036366439d399b4e537ad477b3b2bf5ed8bc1ca2
Bipul Shukla <114582657+BipulShukla12@users.noreply.github.com>
2023-02-26 21:58:51 +0530

    Update Btd.java

--- 100DaysOfJava/src/Btd.java
+++ 100DaysOfJava/src/Btd.java
@@ -1,6 +1,5 @@
 public class Btd{
-  public static void main (String[]args)
-  {
+  public static void main (String[]args){
     int base = 5, 
     x = 3;
     System.out.println ("Required Power is " + power (base, x));
