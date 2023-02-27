b51c977432a2174267aebb8acc755c492abe4fd7
BipulShukla12 <bipulshukla 12@gmail.com>
2023-02-27 21:45:14 +0530

    Day 16

---
+++ 100DaysOfJava/src/Factorial.java
@@ -0,0 +1,17 @@
+class Factorial{
+    // Method to find factorial of the given number
+    static int factorial(int n)
+    {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+    
+    // Driver method
+    public static void main(String[] args)
+    {
+        int num = 6;
+        System.out.println("Factorial of " + num + " is " + factorial(num));
+    }
+}
