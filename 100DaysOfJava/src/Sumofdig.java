b2e80f03a383005e45f5d01319590b387ee2299e
Bipul Shukla <114582657+BipulShukla12@users.noreply.github.com>
2023-03-01 09:06:32 +0530

    Update Sumofdig.java

--- 100DaysOfJava/src/Sumofdig.java
+++ 100DaysOfJava/src/Sumofdig.java
@@ -1,19 +1,16 @@
 public class Sumofdig{
-   public static void main (String[]args)
-   {
 
-     int num = 12345, sum = 0;
+   public static void main (String[]args){
 
-
-
-     
+     int num = 12345, 
+     sum = 0;
      while(num!=0){
+
          sum += num % 10;
          num = num / 10;
      }
-
-     //output
-       System.out.println ("Sum of digits : " + sum);
+  //Print num
+      System.out.println ("Sum of digits : " + sum);
    }
 
 
