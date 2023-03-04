6162996f7ebda1e3ee6b0f57c643477e58f0dec8
Bipul Shukla <114582657+BipulShukla12@users.noreply.github.com>
2023-03-04 19:45:37 +0530

    Update loop.java

--- 100DaysOfJava/src/loop.java
+++ 100DaysOfJava/src/loop.java
@@ -3,9 +3,10 @@ Jiimport java.util.Scanner;
 public class loop {
       public static void main(String[] args) {
            for(int i = 1; i <=5; i +=1){
+// Print the number 1 to 5 .
                   System.out.println( i );
             }
-// Print the no 1 to 5.
+
 
       }
 }
