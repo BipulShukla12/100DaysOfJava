ccb2e0db54f226ad0facde5a9265e87c958fa3cd
Bipul Shukla <114582657+BipulShukla12@users.noreply.github.com>
2023-03-07 14:01:50 +0530

    Update Abundent.java

--- 100DaysOfJava/src/Abundent.java
+++ 100DaysOfJava/src/Abundent.java
@@ -11,7 +11,7 @@ Kipublic class Abundent{
      	System.out.println ("The Abundance is: " + (sum - n));
        }
      else 
-// Print the statement 
+// Print the
        System.out.println (n + " is not an Abundant Number");
    }
  }
