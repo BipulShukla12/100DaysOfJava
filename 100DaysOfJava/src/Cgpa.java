2c896e169f19ded4864687e5a61b6ab5b6b504ac
BipulShukla12 <bipulshukla 12@gmail.com>
2023-03-11 19:17:16 +0530

    cgps

---
+++ 100DaysOfJava/src/Cgpa.java
@@ -0,0 +1,12 @@
+public class Cgpa {
+      public static void main(String[] args) {
+          
+            int subject1 = 5;
+            int subject2 = 25;
+            int subject3 = 45;
+            int cgpa = (subject1 + subject2 + subject3)/3;
+            System.out.println(cgpa);
+
+      }
+      
+}
